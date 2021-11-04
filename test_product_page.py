209b1f31edbd7bb7e9c1299b85e73d9c99cc575e
amaximtsov <amaximtsov@gmail.com>
2021-11-04 15:46:30 +0300

    Add methods:  - test_guest_cant_see_success_message_after_adding_product_to_basket  - test_guest_cant_see_success_message  - test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -23,3 +23,30 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_be_equal_product_name()
     page.should_be_equal_product_price()
+
+
+@pytest.mark.negative
+@pytest.mark.xfail(reason="Will be fixed later")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.negative
+@pytest.mark.xfail(reason="Will be fixed later")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_disappeared_success_message()
